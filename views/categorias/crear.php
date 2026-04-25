d4159b42a2f43ae61d84aa7e725e3e7c1b81b9b4
H3ctor95 <vel231529@uvg.edu.gt>
2026-04-25 03:19:03 -0600

    Se crearon las vista de crear y editar las categorias

---
+++ views/categorias/crear.php
@@ -0,0 +1,6 @@
+<h1>Crear Categoría</h1>
+
+<form method="POST">
+    <input name="nombre" placeholder="Nombre de la categoría"><br>
+    <button>Guardar</button>
+</form>
---
+++ views/categorias/editar.php
@@ -0,0 +1,6 @@
+<h1>Editar Categoría</h1>
+
+<form method="POST">
+    <input name="nombre" value="<?= $categoria->nombre ?>"><br>
+    <button>Actualizar</button>
+</form>
