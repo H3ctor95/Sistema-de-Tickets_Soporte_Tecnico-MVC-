d38c9061da7c8aed94694cc38c4d1dbbbb529430
H3ctor95 <vel231529@uvg.edu.gt>
2026-04-25 03:09:53 -0600

    Se crearon las vistas para poder crear y editar los agentes

---
+++ views/agentes/crear.php
@@ -0,0 +1,7 @@
+<h1>Crear Agente</h1>
+
+<form method="POST">
+    <input name="nombre" placeholder="Nombre"><br>
+    <input name="email" placeholder="Email"><br>
+    <button>Guardar</button>
+</form>
