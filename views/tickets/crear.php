a65b74d29e1e37d9cb21c862d7c03f15ac0dd788
H3ctor95 <vel231529@uvg.edu.gt>
2026-04-25 02:07:52 -0600

    Se crearon la vista de ticket para poder listarlos y tambien se creo la vista de ticket para poder crearlo

---
+++ views/tickets/crear.php
@@ -0,0 +1,7 @@
+<h1>Crear Ticket</h1>
+
+<form method="POST">
+    <input name="titulo" placeholder="Título"><br>
+    <textarea name="descripcion" placeholder="Descripción"></textarea><br>
+    <button>Guardar</button>
+</form>
---
+++ views/tickets/index.php
@@ -0,0 +1,23 @@
+<h1>Tickets</h1>
+
+<a href="index.php?controller=ticket&action=crear">Nuevo Ticket</a>
+
+<table border="1">
+<tr>
+    <th>ID</th>
+    <th>Título</th>
+    <th>Acciones</th>
+</tr>
+
+<?php while($t = $tickets->fetch_object()): ?>
+<tr>
+    <td><?= $t->id ?></td>
+    <td><?= $t->titulo ?></td>
+    <td>
+        <a href="index.php?controller=ticket&action=editar&id=<?= $t->id ?>">Editar</a>
+        <a href="index.php?controller=ticket&action=eliminar&id=<?= $t->id ?>">Eliminar</a>
+    </td>
+</tr>
+<?php endwhile; ?>
+
+</table>
