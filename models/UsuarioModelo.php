f3d808b8a7964eb9c7a5c9f84235cec479d0dc7b
H3ctor95 <vel231529@uvg.edu.gt>
2026-05-01 16:40:43 -0600

    Se creo la estructura de autenticación y su controlador y su modelo

--- models/UsuarioModelo.php
+++ models/UsuarioModelo.php
@@ -3,7 +3,7 @@ require_once "config/conexion.php";
 
 class UsuarioModelo {
 
-    public funtion login($email, $password) {
+    public function login($email, $password) {
         $db = Conexion::conectar();
 
         $sql = "select * from  usuarios where email='$enamil' and contrasena='$password'";
